957d6001992851650036249070e93e426a11d81f
movleax <6619780+movleax@users.noreply.github.com>
2022-04-30 15:05:42 -0700

    WIP

--- gamehub/joesCoolWebsite/urls.py
+++ gamehub/joesCoolWebsite/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('login', views.login),
+    path('', views.index),
+    path('register', views.register),
+    path('create_user', views.create_user)
 ]
--- gamehub/joesCoolWebsite/views.py
+++ gamehub/joesCoolWebsite/views.py
@@ -1,13 +1,63 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
 
 def index(request):
+    # print(request.GET)
+
+    id = request.session['id']#request.GET['userid']
+    # print(queryID)
+
+    user = Users.objects.get(id=id)
+    # print(user)
     context = {
-        "name": "austin",
+        "User": user,
         "favorite_color": "red",
-        "pets": ["tonail", "gabe", "Sir Squash"]
+        "pets": ["tonail", "gabe", "Sir Squash"],
+        "FirstName": user.firstName
     }
     return render(request, "index.html", context)
     #return HttpResponse("this is the equivalent of @app.routes('/')")
+
+def register(request):
+    return render(request, "register.html")
+
+def login(request):
+    if(request.method == "GET"):
+        return render(request, "login.html")
+    if(request.method == "POST"):
+        return attemptLogin(request)
+
+def attemptLogin(request):
+    formEmail = request.POST['email']
+    formPassword = request.POST['password']
+
+    user = Users.objects.filter(email=formEmail, password=formPassword).first()
+
+    if(user == None):
+        print("Login failed")
+        return render(request, "login.html")
+
+    request.session['id'] = user.id
+    
+    return redirect("/")
+    
+
+def create_user(request):
+    print("Get POST info")
+    print(request.POST)
+
+    formEmail = request.POST['email']
+    formPassword = request.POST['password']
+    formFirstName = request.POST['firstName']
+    formLastName = request.POST['lastName']
+
+    user = Users.objects.create(email = formEmail, password = formPassword, firstName = formFirstName, lastName = formLastName)
+
+    print(user)
+
+    request.session['id'] = user.id
+
+    return redirect("/")
