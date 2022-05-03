23fbce186069cfe4754062f0fd3a20d73d3ca6c0
movleax <6619780+movleax@users.noreply.github.com>
2022-05-03 12:32:19 -0700

    move over movleax's changes to duplicate gamehub folder
    delete old gamehub folder

--- gamehub/joesCoolWebsite/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here., Games, Scores
-admin.site.register(Users)
-admin.site.register(Games)
-admin.site.register(Scores)
