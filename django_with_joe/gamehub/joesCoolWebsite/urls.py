1e78f8c269d91882e90f61f9818af4afca125ad6
Joseph Brown <zdragoonmaster@aol.com>
2022-05-03 12:02:21 -0700

    added game

---
+++ django_with_joe/gamehub/joesCoolWebsite/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login', views.login),
+    path('', views.index),
+    path('register', views.register),
+    path('create_user', views.create_user),
+    path('highscores', views.highscores),
+    path('gamepage', views.gamepage),
+    path('playgame', views.playgame),
+]
