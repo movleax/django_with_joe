081ae1b109d00fe910974ca4bf749ac0b5a40033
Joseph Brown <zdragoonmaster@aol.com>
2022-05-05 20:16:03 -0700

    changed login and updated game

---
+++ django_with_joe/django_with_joe/gamehub/joesCoolWebsite/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Users(models.Model):
+    email = models.CharField(max_length=225)
+    password = models.CharField(max_length=225)
+    firstName = models.CharField(max_length=225)
+    lastName = models.CharField(max_length=225)
+    createdat = models.DateTimeField(auto_now_add=True)
+    updateddat = models.DateTimeField(auto_now=True)
+
+class Games(models.Model):
+    name = models.CharField(max_length=225)
+    createdat = models.DateTimeField(auto_now_add=True)
+    updateddat = models.DateTimeField(auto_now=True)
+
+class Scores(models.Model):
+    games_id = models.ForeignKey(Games, on_delete=models.CASCADE, related_name = 'gamescores')
+    users_id = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='userscores')
+    score = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now=True)
+
+class ScoresViewModel():
+    gameName: str
+    userName: str
+    score: int
+    timestamp: str
---
+++ django_with_joe/django_with_joe/gamehub/joesCoolWebsite/views.py
@@ -0,0 +1,120 @@
+from cProfile import run
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from virtualenv import session_via_cli
+from .models import *
+import pygame
+from subprocess import PIPE, run
+
+# Create your views here.
+def index(request):
+    # print(request.GET)
+    
+    id = request.session['id']
+    #request.GET['userid']
+    # print(queryID)
+    user = Users.objects.get(id=id)
+    # print(user)
+    context = {
+        "id":user.id,
+        "User": user,
+        "favorite_color": "red",
+        "pets": ["tonail", "gabe", "Sir Squash"],
+        "FirstName": user.firstName
+    }
+    return render(request, "index.html", context)
+
+        #return HttpResponse("this is the equivalent of @app.routes('/')")
+        
+
+def register(request):
+    return render(request, "register.html")
+
+def login(request):
+    return render(request, "login.html")
+
+
+def attemptLogin(request):
+    if request.method == "POST":
+        formEmail = request.POST['email']
+        formPassword = request.POST['password']
+        user = Users.objects.get(email=formEmail)
+        # if bcrypt.checkpw(request.POST['passwordlogin'].encode(), 
+        #     logged_user.password.encode())
+
+        print("-------------------------------")
+        print(user)
+        print("-------------------------------")
+
+        if(user == None):
+            print("Login failed")
+            return render(request, "login.html")
+        print (user.id)
+        request.session['id'] = user.id
+        return redirect("/login")
+
+    return redirect("/")
+
+def logout(request):
+    del request.session['id']
+    return redirect('/')
+
+def create_user(request):
+    print("Get POST info")
+    print(request.POST)
+    formEmail = request.POST['email']
+    formPassword = request.POST['password']
+    formFirstName = request.POST['firstName']
+    formLastName = request.POST['lastName']
+    user = Users.objects.create(email = formEmail, password = formPassword, firstName = formFirstName, lastName = formLastName)
+    print(user)
+    request.session['id'] = user.id
+    return redirect("/login")
+
+def gamepage (request):
+    return render(request,"gamepage.html")
+
+def playgame (request):
+    return render(request, "rungame.html")
+
+
+def my_game(request):
+    command = ['sudo', 'python test_data.py']
+    result = run(command, stdout=PIPE, stderr=PIPE, shell=True, universal_newlines=True)
+    return render(request, 'mygame.py',{'data1':result})
+
+def highscores(request):
+    id = request.session['id']#request.GET['userid']
+    # gamesList = Games.objects.all()
+    # gamesList.
+    # for game in gameList:
+    scoresList = Scores.objects.all()#.filter(games_id=id)
+    print(scoresList)
+    scoresViewModelDict = {}
+    for score in scoresList:
+        gameName = score.games_id.name
+        scoreVm = ScoresViewModel()
+        scoreVm.gameName = gameName
+        scoreVm.userName = score.users_id.firstName
+        scoreVm.score = score.score
+        scoreVm.timestamp = score.timestamp
+        scoresViewModelDict.setdefault(gameName, []).append(scoreVm)
+    for key, value in scoresViewModelDict.items():
+        print(key, value)
+    #print(scoresViewModelDict)
+    return render(request, "highscores.html", {'Model': scoresViewModelDict })
+
+def grabScore(request):
+    if(request.method == "GET"):
+        return render(request, "rungame.html")
+    if(request.method == "POST"):
+        return highscores(request)
+
+
+def addFavorite(request):
+    favgame = []
+    if (request.method == "GET"):
+        return render(request, "gamepage")
+    if (request.method == "POST"):
+        return Games.name.append.favgame(request, "index", favgame)
+    
